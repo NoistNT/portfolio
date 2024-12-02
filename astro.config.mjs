98d35925ef636e36087a9087299fb053efd84e93
NoistNT <arielgnr23@gmail.com>
2024-12-02 20:38:22 -0300

    Add tailwindcss, shadcn ui and react

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,10 @@
 // @ts-check
-// @ts-check
-import { defineConfig } from 'astro/config';
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind({ applyBaseStyles: false }), react()],
+  vite: { resolve: { alias: { '@': '/src' } } },
+})
