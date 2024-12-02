d76280b2280b86acf8be9aa6aec0fc77825f6ea5
NoistNT <arielgnr23@gmail.com>
2024-12-02 20:40:33 -0300

    Add dark theme

---
+++ src/store/theme.ts
@@ -0,0 +1,14 @@
+type Theme = 'light' | 'dark' | 'system'
+
+export const getInitialTheme = (): Theme => {
+  if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
+    return localStorage.getItem('theme') as Theme
+  }
+  return 'system'
+}
+
+export const getSystemTheme = (): 'light' | 'dark' => {
+  return window.matchMedia('(prefers-color-scheme: dark)').matches
+    ? 'dark'
+    : 'light'
+}
