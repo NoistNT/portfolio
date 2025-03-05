f195390c44f8457a882ca754df7c44ce1a0c13fb
NoistNT <arielgnr23@gmail.com>
2025-03-05 10:56:16 -0300

    Lint files

--- src/store/theme.ts
+++ src/store/theme.ts
@@ -1,14 +1,12 @@
-type Theme = 'light' | 'dark' | 'system'
+type Theme = 'light' | 'dark' | 'system';
 
 export const getInitialTheme = (): Theme => {
   if (typeof localStorage !== 'undefined' && localStorage.getItem('theme')) {
-    return localStorage.getItem('theme') as Theme
+    return localStorage.getItem('theme') as Theme;
   }
-  return 'system'
-}
+  return 'system';
+};
 
 export const getSystemTheme = (): 'light' | 'dark' => {
-  return window.matchMedia('(prefers-color-scheme: dark)').matches
-    ? 'dark'
-    : 'light'
-}
+  return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+};
